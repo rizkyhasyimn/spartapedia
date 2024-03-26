478093894fb1d11d99f21d88f27f07c9c62a80e8
rizkyhasyim <2112500877@student.budiluhur.ac.id>
2024-03-27 04:47:30 +0700

    add dotenv to app.py

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from http import client
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 import requests
 from bs4 import BeautifulSoup
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 
-client = MongoClient('mongodb+srv://Hasyim:learningx@cluster0.oa5m4py.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-db = client.dbsparta
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URL = os.environ.get("MONGODB_URL")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URL)
+db = client[DB_NAME]
 
 app = Flask(__name__)
 
